eb1dabe9f597da49febb5e242ff915737c2c8bb3
Glitched Polygons <glitchedpolygons@protonmail.com>
2022-01-18 00:53:34 +0100

    Added fusion dark palette as base style.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
 #include "mainwindow.h"
 #include "constants.h"
 
+#include <QStyleFactory>
 #include <QApplication>
 #include <QFile>
+
+static inline QPalette getDarkFusionPalette()
+{
+    QPalette palette;
+    palette.setColor(QPalette::Window, QColor(53, 53, 53));
+    palette.setColor(QPalette::WindowText, Qt::white);
+    palette.setColor(QPalette::Disabled, QPalette::WindowText, QColor(127, 127, 127));
+    palette.setColor(QPalette::Base, QColor(32, 32, 32));
+    palette.setColor(QPalette::AlternateBase, QColor(66, 66, 66));
+    palette.setColor(QPalette::ToolTipBase, Qt::white);
+    palette.setColor(QPalette::ToolTipText, Qt::white);
+    palette.setColor(QPalette::Text, Qt::white);
+    palette.setColor(QPalette::Disabled, QPalette::Text, QColor(127, 127, 127));
+    palette.setColor(QPalette::Dark, QColor(35, 35, 35));
+    palette.setColor(QPalette::Shadow, QColor(20, 20, 20));
+    palette.setColor(QPalette::Button, QColor(53, 53, 53));
+    palette.setColor(QPalette::ButtonText, Qt::white);
+    palette.setColor(QPalette::Disabled, QPalette::ButtonText, QColor(127, 127, 127));
+    palette.setColor(QPalette::BrightText, Qt::red);
+    palette.setColor(QPalette::Link, QColor(42, 197, 218));
+    palette.setColor(QPalette::LinkVisited, QColor(12, 116, 130));
+    palette.setColor(QPalette::Highlight, QColor(42, 197, 218));
+    palette.setColor(QPalette::Disabled, QPalette::Highlight, QColor(80, 80, 80));
+    palette.setColor(QPalette::HighlightedText, Qt::white);
+    palette.setColor(QPalette::Disabled, QPalette::HighlightedText, QColor(127, 127, 127));
+    return palette;
+}
 
 int main(int argc, char* argv[])
 {
@@ -14,6 +42,8 @@ int main(int argc, char* argv[])
 
     QApplication application(argc, argv);
     application.setWindowIcon(QIcon(":/img/icon.png"));
+    application.setStyle(QStyleFactory::create("Fusion"));
+    application.setPalette(getDarkFusionPalette());
 
     QFile theme(QCoreApplication::applicationDirPath() + "/theme.qss");
     if (theme.exists())
